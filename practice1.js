6989bfa753fd825ef028462f145ee6dbde740678
BlueInked <BlueInked@users.noreply.github.com>
2017-02-17 20:43:17 +0300

    Playing with objects
    A function isn't needed to add to an object, but the question called for one...

---
+++ practice1.js
@@ -0,0 +1,15 @@
+var grades = {
+	english : 100,
+	math : 76,
+	science : 88,
+	history : 93,
+	physEd : 55,
+	music : 90,
+	art : 85
+};
+
+var function addGrades(subject, grade) {
+	grades.subject = "grade";
+};
+
+addGrades(drama, 77);
