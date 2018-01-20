68ff392cbc2c76b71643e712282324fc7ed2f99a
BlueInked <BlueInked@users.noreply.github.com>
2018-01-20 12:11:55 +0300

    Classes and Import/Export Example

---
+++ ECMAscript/index.js
@@ -0,0 +1,14 @@
+import Animal from './Animal';
+class Lion extends Animal {
+  constructor(name, height, color) {
+    super(name, height);
+    this.color = color;
+  }
+
+  hello() {
+    console.log(`Hi! I'm ${this.name} from Pride Rock!`)
+  }
+}
+
+let son = new Lion ("Simba", 2, "golden");
+son.hello();
